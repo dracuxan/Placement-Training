0ce2248327cc206b277baa05de907bac58abcf55
Nisarg <khodkenisarg@gmail.com>
2025-02-19 15:00:08 +0530

    upd

---
+++ class/lec-19_02_25.py
@@ -0,0 +1,16 @@
+# 1
+for i in range(10):
+    print(i + 1)
+
+# 2
+for i in range(10):
+    print((i + 1) ** 2)
+
+# 3
+n = int(input("::"))
+sum1 = 0
+for i in range(n + 1):
+    sum1 += i
+print(sum1)
+
+# 4
